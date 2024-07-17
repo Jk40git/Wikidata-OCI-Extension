3d12e0985a59def8fc59651b6350385f957be984
Dorivn2010 <kevinadjei4@gmail.com>
2024-07-17 19:38:18 +0000

    Dark mode update

--- Popup/popup.js
+++ Popup/popup.js
@@ -7,3 +7,8 @@ document.getElementById('zoom-out').addEventListener('click', function()
 {
     document.body.style.zoom = parseFloat(document.body.style.zoom || 1) - 0.1;
 });
+
+document.getElementById('dark-mode-toggle').addEventListener('click', function() 
+{
+    document.body.classList.toggle('dark-mode');
+});
