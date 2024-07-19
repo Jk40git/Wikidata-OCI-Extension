5a86e2340486105d50efa54e1a099bd63b9f003c
Jacob Kondo <jacobkondo4@gmail.com>
2024-07-19 21:10:40 +0000

    Sample (#1)
    * Zoom buttons
    
    * Dark mode
    
    * Dark mode update
    
    * Close button update
    
    * Update
    
    * New update
    
    * Style Update
    
    * Zoom button update
    
    * Close button update
    
    * update css
    
    * Zoom update
    
    * Hyperlink update
    
    * Update css and html
    
    * Update
    
    * update css
    
    * update css
    
    * update css
    
    * update css
    
    * update js
    
    ---------
    
    Co-authored-by: Dorivn2010 <kevinadjei4@gmail.com>

--- Popup/popup.js
+++ Popup/popup.js
@@ -14,3 +14,7 @@ document.getElementById('zoom-out').addEventListener('click', function()
     document.body.style.zoom = parseFloat(document.body.style.zoom || 1) - 0.1;
 });
 
+document.getElementById('dark-mode-toggle').addEventListener('click', function() 
+{
+    document.body.classList.toggle('dark-mode');
+});
