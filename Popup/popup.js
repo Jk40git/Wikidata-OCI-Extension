36936451c6073bdb638cffc2cbf5e666510b2c95
JK40git <jacobkondo4@gmail.com>
2024-07-19 21:22:46 +0000

    comment on dark mode

--- Popup/popup.js
+++ Popup/popup.js
@@ -14,6 +14,7 @@ document.getElementById('zoom-out').addEventListener('click', function()
     document.body.style.zoom = parseFloat(document.body.style.zoom || 1) - 0.1;
 });
 
+// Interact with the dark mode icon
 document.getElementById('dark-mode-toggle').addEventListener('click', function() 
 {
     document.body.classList.toggle('dark-mode');
