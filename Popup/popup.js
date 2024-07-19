44373ea715cc6c2b8ea42987d481a75ef7683f4a
JK40git <jacobkondo4@gmail.com>
2024-07-19 07:03:39 +0000

    get the currently active tab

--- Popup/popup.js
+++ Popup/popup.js
@@ -1,3 +1,9 @@
+// get the currently active tab in the current window
+// and then invoke the callback function gotTabs.
+let query = { active: true, currentWindow: true };
+chrome.tabs.query(query, gotTabs);
+
+
 document.getElementById('zoom-in').addEventListener('click', function() 
 {
     document.body.style.zoom = parseFloat(document.body.style.zoom || 1) + 0.1;
@@ -7,3 +13,4 @@ document.getElementById('zoom-out').addEventListener('click', function()
 {
     document.body.style.zoom = parseFloat(document.body.style.zoom || 1) - 0.1;
 });
+
