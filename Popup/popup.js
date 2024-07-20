65cd4ec96084b42c396c9b2a66b2a28c370cc634
Dorivn2010 <kevinadjei4@gmail.com>
2024-07-20 20:47:55 +0000

    Dark mode update

--- Popup/popup.js
+++ Popup/popup.js
@@ -14,7 +14,31 @@ document.getElementById('zoom-out').addEventListener('click', function()
     document.body.style.zoom = parseFloat(document.body.style.zoom || 1) - 0.1;
 });
 
-document.getElementById('dark-mode-toggle').addEventListener('click', function() 
-{
-    document.body.classList.toggle('dark-mode');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+    //Function to initialize dark mode based on localStorage
+    const initializeDarkMode = () => {
+        if(localStorage.getItem('dark-mode') === 'enabled') {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    };
+
+    //Function to toggle dark mode
+    const toggleDarkMode = () => {
+        document.body.classList.toggle('dark-mode');
+        if(document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('dark-mode', 'enabled');
+        } else {
+            localStorage.removeItem('dark-mode');
+        }
+    };
+
+    //Initialize dark mode based on localStorage
+    initializeDarkMode();
+
+    //Add event listener for dark mode toggle button
+    darkModeToggle.addEventListener('click', toggleDarkMode);
 });
