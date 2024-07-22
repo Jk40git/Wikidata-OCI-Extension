1675b65ff52cd2c5dca9848caa99d271b5da8099
Dorivn2010 <kevinadjei4@gmail.com>
2024-07-22 00:40:27 +0000

    Dark mode implementation

--- Popup/popup.js
+++ Popup/popup.js
@@ -14,31 +14,13 @@ document.getElementById('zoom-out').addEventListener('click', function()
     document.body.style.zoom = parseFloat(document.body.style.zoom || 1) - 0.1;
 });
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
+const themeToggleBtn = document.querySelector('.theme-toggle');
 
-    //Function to initialize dark mode based on localStorage
-    const initializeDarkMode = () => {
-        if(localStorage.getItem('dark-mode') === 'enabled') {
-            document.body.classList.add('dark-mode');
-        } else {
-            document.body.classList.remove('dark-mode');
-        }
-    };
-
-    //Function to toggle dark mode
-    const toggleDarkMode = () => {
-        document.body.classList.toggle('dark-mode');
-        if(document.body.classList.contains('dark-mode')) {
-            localStorage.setItem('dark-mode', 'enabled');
-        } else {
-            localStorage.removeItem('dark-mode');
-        }
-    };
-
-    //Initialize dark mode based on localStorage
-    initializeDarkMode();
-
-    //Add event listener for dark mode toggle button
-    darkModeToggle.addEventListener('click', toggleDarkMode);
+themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        localStorage.removeItem('theme');
+    }
 });
