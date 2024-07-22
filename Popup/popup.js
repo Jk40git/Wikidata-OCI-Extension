d9a6e04a9a7db0754a16a03afc394ff67b47862c
Dorivn2010 <kevinadjei4@gmail.com>
2024-07-22 17:53:47 +0000

    Dark mode update

--- Popup/popup.js
+++ Popup/popup.js
@@ -16,6 +16,10 @@ document.getElementById('zoom-out').addEventListener('click', function()
 
 const themeToggleBtn = document.querySelector('.theme-toggle');
 
+const theme = localStorage.getItem('theme');
+
+theme && document.body.classList.add(theme);
+
 themeToggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     if (document.body.classList.contains('dark-mode')) {
