af2eb616a07e1a90db63c9360688a87bc63e681d
JK40git <jacobkondo4@gmail.com>
2024-08-01 00:26:25 +0000

    Add highlight event listener

--- page.js
+++ page.js
@@ -6,12 +6,13 @@ function handleSelection() {
     // get the selected text
     word = window.getSelection();
     selectedWord = word.toString().toLowerCase().trim();
+    console.log("worked")
 }
 
 // receive the word from popup
 chrome.runtime.onMessage.addListener(gotOutput);
 
-function gotOutput() {
+const data = function gotOutput() {
     let outPut =
         selectedWord && selectedWord.length > 0
             ? selectedWord = document.getElementById("word")
@@ -19,4 +20,12 @@ function gotOutput() {
 
     // send the output to the server
     sendResponse({ hword: outPut })
+    console.log("worked")
+    return outPut
+
 }
+
+// fetchEntities(data())
+//     // fetchEntities('Cat', 'en')
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
