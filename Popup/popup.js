743714b165d1d7c51776dad745b952fe5b4791a5
JK40git <jacobkondo4@gmail.com>
2024-08-11 23:48:42 +0000

    add Dark mode and Zoom

--- Popup/popup.js
+++ Popup/popup.js
@@ -1,4 +1,20 @@
 /** @format */
+// Interaction with the zoom button
+document.getElementById('zoom-in').addEventListener('click', function () {
+  document.body.style.zoom = parseFloat(document.body.style.zoom || 1) + 0.1;
+});
+
+document.getElementById('zoom-out').addEventListener('click', function () {
+  document.body.style.zoom = parseFloat(document.body.style.zoom || 1) - 0.1;
+});
+
+// Dark mode interaction
+var content = document.getElementsByTagName('body')[0];
+var darkMode = document.getElementById('dark-mode-toggle');
+darkMode.addEventListener('click', () => {
+  darkMode.classList.toggle('active');
+  content.classList.toggle('night');
+})
 
 async function fetchEntities(searchTerm, language = 'en') {
   const endpoint = 'https://www.wikidata.org/w/api.php';
