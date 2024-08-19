c66502285864db9cf8e920eac5afda22d29b6d50
JK40git <jacobkondo4@gmail.com>
2024-08-19 01:12:31 +0000

    cleaning up codebase

--- content.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('mouseup', function() {
-    const selectedText = window.getSelection().toString();
-    if (selectedText) {
-      chrome.runtime.sendMessage({ text: selectedText });
-    }
-  });
-  
